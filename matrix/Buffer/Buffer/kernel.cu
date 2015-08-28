3f80acd04449d31242f0cdda54ed7aea461cc859
Yuzo <hyzgog@gmail.com>
2015-08-29 01:01:09 +0800

    improve structure

--- matrix/Buffer/Buffer/kernel.cu
+++ matrix/Buffer/Buffer/kernel.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/timer.hpp>
 #include <boost/format.hpp>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -8,6 +9,7 @@ using namespace boost;
 
 __global__ void multiplyKernel(const float* A, const float* B, float* C, const unsigned int R);
 cudaError_t multiplyWithCuda(const float* A, const float* B, float* C, const unsigned int R);
+void logTime(const string& message);
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
@@ -15,16 +17,21 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
+	logTime("Launched");
 	const int R = stoi(argv[1]);
 
 	float* A = new float[R * R];
 	float* B = new float[R * R];
 	float* C = new float[R * R];
 
+	logTime("Host memory allocated");
+
 	for (int i = 0; i < R * R; i++) {
-		A[i] = rand() % 100;
-		B[i] = rand() % 100;
+		A[i] = float(rand() % 100);
+		B[i] = float(rand() % 100);
 	}
+
+	logTime("Matrix randomly filled");
 
 	// Add vectors in parallel.
 	cudaError_t cudaStatus = multiplyWithCuda(A, B, C, R);
@@ -33,7 +40,8 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	cout << format("Result Matrix Pointer: %1%") % C << endl;
+	logTime("Back to host");
+	cout << format("Result Matrix Pointer: %1$x") % C << endl;
 
 	// cudaDeviceReset must be called before exiting in order for profiling and
 	// tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -47,6 +55,8 @@ int main(int argc, char* argv[]) {
 	delete[] B;
 	delete[] C;
 
+	logTime("Finish");
+
 	return EXIT_SUCCESS;
 }
 
@@ -56,7 +66,7 @@ __global__ void multiplyKernel(const float* A, const float* B, float* C, const u
 	float sum = 0;
 
 	for (int i = 0; i < R; i++) {
-		sum += A[row * R + i] * B[row * R + i];
+		sum += A[row * R + i] * B[i * R + col];
 	}
 
 	C[row * R + col] = sum;
@@ -69,74 +79,54 @@ cudaError_t multiplyWithCuda(const float* A, const float* B, float* C, const uns
 	float* devC = NULL;
 	cudaError_t cudaStatus;
 
-	// Choose which GPU to run on, change this on a multi-GPU system.
-	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-		goto Error;
+	try {
+		cudaStatus = cudaSetDevice(0);
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaSetDevice failed! Do you have a CUDA-capable GPU installed?");
+
+		cudaStatus = cudaMalloc((void**)&devA, R * R * sizeof(float));
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMalloc failed!");
+
+		cudaStatus = cudaMalloc((void**)&devB, R * R * sizeof(float));
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMalloc failed!");
+
+		cudaStatus = cudaMalloc((void**)&devC, R * R * sizeof(float));
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMalloc failed!");
+
+		cudaStatus = cudaMemcpy(devA, A, R * R * sizeof(float), cudaMemcpyHostToDevice);
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMemcpy failed!");
+
+		cudaStatus = cudaMemcpy(devB, B, R * R * sizeof(float), cudaMemcpyHostToDevice);
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMemcpy failed!");
+
+		logTime("Ready to launch kernel");
+		timer t;
+		multiplyKernel << <R, R >> >(devA, devB, devC, R);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess) throw runtime_error((format("addKernel launch failed: %1%") % (cudaGetErrorString(cudaStatus))).str());
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess) throw runtime_error((format("cudaDeviceSynchronize returned error code %1% after launching addKernel!") % cudaStatus).str());
+
+		logTime("Kernel finish");
+		double gflops = pow(R, 3) / 536870912 / t.elapsed();
+		cout << format("%1% GFLOPS / %2%s Computing Time") % gflops % t.elapsed() << endl;
+
+		cudaStatus = cudaMemcpy(C, devC, R * R * sizeof(float), cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess) throw runtime_error("cudaMemcpy failed!");
+
+	} catch (const runtime_error& e) {
+		cerr << e.what() << endl;
 	}
 
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&devA, R * R * sizeof(float));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&devB, R * R * sizeof(float));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&devC, R * R * sizeof(float));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(devA, A, R * R * sizeof(float), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMemcpy(devB, B, R * R * sizeof(float), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	// Launch a kernel on the GPU with one thread for each element.
-	multiplyKernel << <R, R >> >(devA, devB, devC, R);
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
-	}
-
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-		goto Error;
-	}
-
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(C, devC, R * R * sizeof(float), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-Error:
 	cudaFree(devA);
 	cudaFree(devB);
 	cudaFree(devC);
 
 	return cudaStatus;
+}
+
+void logTime(const string& message) {
+	static timer t;
+	cout << boost::format("[%1$.3f] %2%") % t.elapsed() % message << endl;
 }
